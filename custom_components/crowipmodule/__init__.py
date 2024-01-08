d0ece744663ea89bda5d81b4444c2b928fa93475
acdcnow <33558339+acdcnow@users.noreply.github.com>
2024-01-08 13:37:11 +0100

    Update __init__.py
    const.py introduced

--- custom_components/crowipmodule/__init__.py
+++ custom_components/crowipmodule/__init__.py
@@ -13,33 +13,31 @@ from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "crowipmodule"
-
-DATA_CRW = "crowipmodule"
-
-CONF_CODE = "code"
-CONF_CROW_KEEPALIVE = "keepalive_interval"
-CONF_CROW_PORT = "port"
-CONF_AREANAME = "name"
-CONF_AREAS = "areas"
-CONF_ZONENAME = "name"
-CONF_ZONES = "zones"
-CONF_ZONETYPE = "type"
-CONF_OUTPUTS = "outputs"
-CONF_OUTPUTNAME = "name"
-
-DEFAULT_PORT = 5002
-DEFAULT_KEEPALIVE = 60
-"""CDEFAULT_KEEPALIVE = 60"""
-DEFAULT_ZONETYPE = "opening"
-DEFAULT_TIMEOUT = 10
-"""DEFAULT_TIMEOUT = 10"""
-
-SIGNAL_ZONE_UPDATE = "crowipmodule.zones_updated"
-SIGNAL_AREA_UPDATE = "crowipmodule.areas_updated"
-SIGNAL_SYSTEM_UPDATE = "crowipmodule.system_updated"
-SIGNAL_OUTPUT_UPDATE = "crowipmodule.output_updated"
-SIGNAL_KEYPAD_UPDATE = "crowipmodule.keypad_updated"
+from .const import (
+    DOMAIN,
+    DATA_CRW,
+    CONF_CODE,
+    CONF_CROW_KEEPALIVE,
+    CONF_CROW_PORT,
+    CONF_AREANAME,
+    CONF_AREAS,
+    CONF_ZONENAME,
+    CONF_ZONES,
+    CONF_ZONETYPE,
+    CONF_OUTPUTS,
+    CONF_OUTPUTNAME,   
+    DEFAULT_PORT,
+    DEFAULT_KEEPALIVE,
+    """CDEFAULT_KEEPALIVE = 60"""
+    DEFAULT_ZONETYPE,
+    DEFAULT_TIMEOUT,
+    """DEFAULT_TIMEOUT = 10"""    
+    SIGNAL_ZONE_UPDATE,
+    SIGNAL_AREA_UPDATE,
+    SIGNAL_SYSTEM_UPDATE,
+    SIGNAL_OUTPUT_UPDATE,
+    SIGNAL_KEYPAD_UPDATE,
+)
 
 OUTPUT_SCHEMA = vol.Schema(
     {
