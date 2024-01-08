40ab96405d92ba8d8ea1aab0a075f2ca8a94146c
acdcnow <33558339+acdcnow@users.noreply.github.com>
2024-01-08 13:38:12 +0100

    Update const.py

--- custom_components/crowipmodule/const.py
+++ custom_components/crowipmodule/const.py
@@ -1,8 +1,5 @@
-
 DOMAIN = "crowipmodule"
-
 DATA_CRW = "crowipmodule"
-
 CONF_CODE = "code"
 CONF_CROW_KEEPALIVE = "keepalive_interval"
 CONF_CROW_PORT = "port"
@@ -13,14 +10,10 @@ CONF_ZONES = "zones"
 CONF_ZONETYPE = "type"
 CONF_OUTPUTS = "outputs"
 CONF_OUTPUTNAME = "name"
-
 DEFAULT_PORT = 5002
 DEFAULT_KEEPALIVE = 60
-"""CDEFAULT_KEEPALIVE = 60"""
 DEFAULT_ZONETYPE = "opening"
 DEFAULT_TIMEOUT = 10
-"""DEFAULT_TIMEOUT = 10"""
-
 SIGNAL_ZONE_UPDATE = "crowipmodule.zones_updated"
 SIGNAL_AREA_UPDATE = "crowipmodule.areas_updated"
 SIGNAL_SYSTEM_UPDATE = "crowipmodule.system_updated"
